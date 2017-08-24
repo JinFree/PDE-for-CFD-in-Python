75e295814c21a990e2b5e5404ae6c115d2ee3d67
Jinyeong Wang <ytrqwe12@gmail.com>
2017-08-24 09:54:34 +0900

    Cavity Fix Bug

--- Cavity.py
+++ Cavity.py
@@ -11,7 +11,7 @@ class Cavity:
         self.dx = 1.0 / float(self.Grid-1)
         self.dt = 0.0005
         self.U0 = 1.0
-        self.Ermax = 0.00007
+        self.Ermax = 0.00005
         self.U = np.random.randn(self.Grid, self.Grid)
         self.V = np.random.randn(self.Grid, self.Grid)
         self.W = np.random.randn(self.Grid, self.Grid)
@@ -49,6 +49,7 @@ class Cavity:
             shutil.rmtree(self.dirname)
         os.mkdir(self.dirname)
         return
+
     def Tec_Write(self):
         filename = "{0}/{1}, iter = {2}.dat".format(self.dirname, self.Scheme_name, self.iter)
         file = open(filename, 'w')
@@ -56,8 +57,8 @@ class Cavity:
         file.write("zone i=%d j=%d\n" % (self.Grid, self.Grid))
         for i in range(self.Grid):
             for j in range(self.Grid):
-                data = "%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\n" % \
-                       (float(j) * self.dx, float(i) * self.dx, 0.0, self.U[i][j],
+                data = "%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\t%6.6f\n" % \
+                       (float(j) * self.dx, float(i) * self.dx, self.U[i][j],
                         self.V[i][j], self.W[i][j], self.Psi[i][j])
                 file.write(data)
         file.close()
@@ -84,57 +85,60 @@ class Cavity:
         self.Psi = np.zeros((self.Grid, self.Grid))
         self.Psinew = np.zeros((self.Grid, self.Grid))
         for i in range(self.Grid):
-            self.U[self.Grid-1][i] = self.U0
+            self.U[i][self.Grid-1] = self.U0
         return
 
-    def Boundary_Condition(self):
+    def Boundary_Condition(self):  # [x][y]
         for i in range(self.Grid):
-            self.W[i][self.Grid-1] = 2.0 * (self.Psi[i][self.Grid-1] - self.Psi[i][self.Grid-2]) / math.pow(self.dx, 2.0)
-            self.W[i][0] = 2.0 * (self.Psi[i][0] - self.Psi[i][2]) / math.pow(self.dx, 2.0)
-            self.W[self.Grid-1][i] = 2.0 * (self.Psi[self.Grid-1][i] - self.Psi[self.Grid-2][i]) / math.pow(self.dx, 2.0) - 2.0 * self.U0 / self.dx
+            self.W[self.Grid-1][i] = 2.0 * (self.Psi[self.Grid-1][i] - self.Psi[self.Grid-2][i]) \
+                                     / math.pow(self.dx, 2.0)
             self.W[0][i] = 2.0 * (self.Psi[0][i] - self.Psi[1][i]) / math.pow(self.dx, 2.0)
+            self.W[i][self.Grid-1] = 2.0 * (self.Psi[i][self.Grid-1] - self.Psi[i][self.Grid-2]) \
+                                     / math.pow(self.dx, 2.0) - (2.0 * self.U0) / self.dx
+            self.W[i][0] = 2.0 * (self.Psi[i][0] - self.Psi[i][1]) / math.pow(self.dx, 2.0)
         return
 
-    def Velocity(self):
-        for i in range(1, self.Grid-1):
-            for j in range(1, self.Grid-1):
-                self.U[i][j] = (self.Psi[i+1][j] - self.Psi[i-1][j])/(2.0*self.dx)
-                self.V[i][j] = (self.Psi[i][j+1] - self.Psi[i][j-1])/(2.0*self.dx)
+    def Velocity(self):  # [x][y]
+        for j in range(1, self.Grid-1):
+            for i in range(1, self.Grid-1):
+                self.U[i][j] = (self.Psi[i][j+1] - self.Psi[i][j-1])/(2.0*self.dx)
+                self.V[i][j] = -(self.Psi[i+1][j] - self.Psi[i-1][j])/(2.0*self.dx)
         return
 
     def Time_Marching(self):
         self.W = self.Wnew.copy()
         return
 
-    def Check_W_Error(self):
-        for i in range(1, self.Grid-1):
-            for j in range(1, self.Grid-1):
+    def Check_W_Error(self):  # [x][y]
+        for j in range(1, self.Grid-1):
+            for i in range(1, self.Grid-1):
                 error = abs(self.W[i][j] - self.Wnew[i][j])
                 self.VorticityError += math.pow(error, 2.0)
         self.VorticityError = math.sqrt(self.VorticityError / math.pow(float(self.Grid-2), 2.0))
         return
 
-    def Check_Psi_Error(self):
-        for i in range(1, self.Grid-1):
-            for j in range(1, self.Grid-1):
+    def Check_Psi_Error(self):  # [x][y]
+        for j in range(1, self.Grid-1):
+            for i in range(1, self.Grid-1):
                 error = abs(self.Psi[i][j] - self.Psinew[i][j])
                 self.StreamError += math.pow(error, 2.0)
         self.StreamError = math.sqrt(self.StreamError / math.pow(float(self.Grid-2), 2.0))
         return
 
-    def Vorticity_FTCS(self):
-        for i in range(1, self.Grid-1):
-            for j in range(1, self.Grid-1):
+    def Vorticity_FTCS(self):  # [x][y]
+        for j in range(1, self.Grid-1):
+            for i in range(1, self.Grid-1):
                 self.Wnew[i][j] = (1.0 - 4.0 * self.d) * self.W[i][j]\
-                                  + (self.d - self.c * self.U[i][j+1]) * self.W[i][j+1]\
-                                  + (self.d + self.c * self.U[i][j-1]) * self.W[i][j-1]\
-                                  + (self.d - self.c * self.V[i+1][j]) * self.W[i+1][j]\
-                                  + (self.d + self.c * self.V[i-1][j]) * self.W[i-1][j]
+                                  + (self.d - self.c * self.U[i+1][j]) * self.W[i+1][j]\
+                                  + (self.d + self.c * self.U[i-1][j]) * self.W[i-1][j]\
+                                  + (self.d - self.c * self.V[i][j+1]) * self.W[i][j+1]\
+                                  + (self.d + self.c * self.V[i][j-1]) * self.W[i][j-1]
 
         return
-    def PGS(self):
-        for i in range(1, self.Grid-1):
-            for j in range(1, self.Grid-1):
+
+    def PGS(self):  # [x][y]
+        for j in range(1, self.Grid-1):
+            for i in range(1, self.Grid-1):
                 self.Psinew[i][j] = 0.25 * (self.Psi[i+1][j] + self.Psi[i-1][j] + self.Psi[i][j+1] + self.Psi[i][j-1]
                                             + math.pow(self.dx, 2.0) * self.W[i][j])
     def Stream_PGS(self):
@@ -160,6 +164,7 @@ class Cavity:
             self.Check_W_Error()
             self.Time_Marching()
             self.time += self.dt
+            self.iter += 1
             print("\rtime = %6.6f error = %6.6f" % (self.time, self.VorticityError), end="")
         print()
         self.Para_Write()
